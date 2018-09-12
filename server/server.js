3c9d438cb8474bb9dfd3a3ec9b355776709259cb
Kalpesh Benare <GS-1903@GSLAB.COM>
2018-09-12 12:42:19 +0530

    integrating sockeIo with express app

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './../public');
 var app = express();
+var server = http.createServer(app);
 const port = process.env.PORT || 3050;
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+
+server.listen(port, () => {
   console.log('server started on ', port);
 });
